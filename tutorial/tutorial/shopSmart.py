593396092199c868e23253186557c8d75440fa91
Isamu <isamu.poy@mail.utoronto.ca>
2021-01-16 20:58:18 +0900

    fruitShop

--- tutorial/tutorial/shopSmart.py
+++ tutorial/tutorial/shopSmart.py
@@ -29,13 +29,18 @@ def shopSmart(orderList, fruitShops):
         fruitShops: List of FruitShops
     """
     "*** YOUR CODE HERE ***"
-    return None
+    t_cost = []
+    for shop in fruitShops:
+        t_cost.append(shop.getPriceOfOrder(orderList))
+    return min(t_cost)
 
 if __name__ == '__main__':
   "This code runs when you invoke the script from the command line"
   orders = [('apples',1.0), ('oranges',3.0)]
+
   dir1 = {'apples': 2.0, 'oranges':1.0}
   shop1 =  shop.FruitShop('shop1',dir1)
+
   dir2 = {'apples': 1.0, 'oranges': 5.0}
   shop2 = shop.FruitShop('shop2',dir2)
   shops = [shop1, shop2]
