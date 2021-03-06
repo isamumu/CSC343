cccd987648761ce2b35f4d5119a74308dd35d9df
Poy Isamu Arthur <poyisamu@teach.cs.toronto.edu>
2021-03-06 07:23:59 -0500

    coded q1, q2 in progress

--- A2/sql_templates/q1.sql
+++ A2/sql_templates/q1.sql
@@ -18,6 +18,24 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here:
 
+-- TODO: check Everyone and Duplicate conditions
+-- find a relation relating a passenger to their flight number
+CREATE VIEW passengers AS 
+SELECT booking.flight_id as flight_id, passenger.id as pass_id
+FROM booking JOIN passenger on booking.pass_id = passenger.id;
+
+-- find a relation relating a passenger to their airlines
+CREATE VIEW airPassenger AS
+SELECT passengers.pass_id as pass_id, flight.airline as airline 
+FROM passengers JOIN flight on passengers.flight_id = flight.id;
+
+-- find a relation relating a passenger to their firstname and lastname, with their passenger id 
+CREATE VIEW travellers AS
+SELECT passengers.pass_id as pass_id, passenger.firstname as firstname, passenger.surname as lastname
+FROM passengers JOIN passenger on passengers.pass_id = passenger.id;
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q1
+(SELECT travellers.pass_id as pass_id, travellers.firstname || ' ' || travellers.lastname as name, count(airPassenger.airline) as airlines
+FROM travellers JOIN airPassenger on travellers.pass_id = airPassenger.pass_id
+GROUP BY travellers.pass_id, travellers.firstname, travellers.lastname);
--- A2/sql_templates/q2.sql
+++ A2/sql_templates/q2.sql
@@ -20,6 +20,63 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here:
 
+-- RULE: domestic flights --> 35% refund for delay >= 5hrs 
+--                        --> 50% refund for delay >= 10hrs
+--       international flights --> 35% refund for delay >= 8hrs
+--                             --> 50% refund for delay >= 12hrs
+-- BUT: if the pilot arrives at most 50% of the departure delay, refunds are dropped :( 
+
+CREATE VIEW outCountries AS
+SELECT flight.id as id, airport.country as outCountry 
+FROM flight JOIN airport on flight.outbound = airport.code;
+
+CREATE VIEW inCountries AS
+SELECT flight.id as id, airport.country as inCountry 
+FROM flight JOIN airport on flight.inbound = airport.code;
+
+CREATE VIEW flightCountry AS
+SELECT outCountries.id as flight_id, outCountries.outCountry as outCountry, inCountries.inCountry as inCountry
+FROM outCountries JOIN inCountries on outCountries.id = inCountries.id;
+
+-- find table of international flights
+CREATE VIEW internationals AS
+SELECT flight_id 
+FROM flightCountry 
+WHERE outCountry <> inCountry;
+
+-- find table of domestic flights
+CREATE VIEW domestics AS
+SELECT flight_id 
+FROM flightCountry 
+WHERE outCountry = inCountry;
+
+-- relate flights to times for domestic 
+
+-- relate flights to times for international
+
+-- find flights that did not make up for the delay
+
+-- find pricing of international flights
+
+-- find pricing of domestic flights
+
+-- find domestic flights of 35% refund
+
+-- find domestic flights of 50% refund
+
+-- find international flights of 35% refund
+
+-- find international flights of 50% refund
+
+-- find 35% refund domestic flights that were booked relating to type of seat and find the refunded price
+
+-- find 50% refund domestic flights that were booked relating to type of seat and find the refunded price
+
+-- find 35% refund international flights that were booked relating to type of seat and find the refunded price
+
+-- find 50% refund international flights that were booked relating to type of seat and find the refunded price
+
+-- relate above 4 tables and group by code, name, year, and sum the refunds over the seat class attributes
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q2
