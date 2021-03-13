30b9f2877b562d3bba65e117fbe945499cf5c16c
Poy Isamu Arthur <poyisamu@teach.cs.toronto.edu>
2021-03-12 22:06:23 -0500

    fixing q5

--- A2/sql_templates/q5.sql
+++ A2/sql_templates/q5.sql
@@ -33,10 +33,10 @@ SELECT id, outbound, inbound, s_dep, s_arv
 FROM flight;
 
 -- list of all available flights with a 24 hour interval (do a self join)
-CREATE VIEW inOut24 AS
-SELECT f1.inbound as airport, (f2.s_dep - f1.s_arv) as timeLeft, f2.outbound as outAirport
-FROM flight f1 join flight f2 on f1.outbound = f2.inbound 
-WHERE (f1.s_dep - f1.s_arv < '24:00:00') and (f2.s_dep - f1.s_arv > '00:00:00') and f2.outbound = f1.inbound and f1.id <> f2.id and f1.s_dep >= (SELECT day from day);
+-- CREATE VIEW inOut24 AS
+-- SELECT f1.inbound as airport, (f2.s_dep - f1.s_arv) as timeLeft, f2.outbound as outAirport
+-- FROM flight f1 join flight f2 on f1.outbound = f2.inbound 
+-- WHERE (f1.s_dep - f1.s_arv < '24:00:00') and (f2.s_dep - f1.s_arv > '00:00:00') and f2.outbound = f1.inbound and f1.id <> f2.id and f1.s_dep >= (SELECT day from day);
 
 -- find all available flights from Toronto Pearson Airport
 -- ASSUMPTION: any day on or after the day of interest is valid, because one can simply wait for the flight to leave eventually
@@ -50,13 +50,15 @@ WHERE outbound = 'YYZ'
 
 -- HINT: You can answer the question by writing one recursive query below, without any more views.
 -- Your query that answers the question goes below the "insert into" line:
+-- NOTE: we check whether the delay is greater than 00:00:00 because there is a chance that the plane has already departed by the time of arrival.
+--       Therefore, we must filter out such cases where the buffer time is negative
 INSERT INTO q5
 WITH RECURSIVE hopping AS (
-	(SELECT 1 as num_flights, inbound as destination, s_arv as arrivals FROM YYZflights)
+	(SELECT 1 as num_flights, inbound as destination, s_arv as arrivals, s_dep as dep FROM YYZflights)
 	UNION all
-	(SELECT num_flights + 1, inbound as destination, s_arv as arrivals
+	(SELECT num_flights + 1, inbound as destination, hopping.arrivals, s_dep as dep
 	FROM hopping JOIN flights on hopping.destination = flights.outbound
-	WHERE num_flights < (SELECT n from n) and s_dep - arrivals < '24:00:00')
+	WHERE num_flights < (SELECT n from n) and s_dep - arrivals < '24:00:00' and s_dep - arrivals > '00:00:00')
 ) 
 SELECT destination, num_flights
 FROM hopping
